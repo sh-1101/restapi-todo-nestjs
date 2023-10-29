13b2a65257736c1fc6ff767dba92292a187c83eb
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-10-29 22:30:57 +0900

    User Service/Controller

--- restapi-todo-nestjs/src/user/user.controller.ts
+++ restapi-todo-nestjs/src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client';
 
+@UseGuards(AuthGuard('jwt')) //userの全てのエンドポイントをjwtでプロテクト
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    //Omit Userのデータ型からhashedPasswordを取り除いたもの
+    return req.user; //validateのreturnのユーザーオブジェクトを返す
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
