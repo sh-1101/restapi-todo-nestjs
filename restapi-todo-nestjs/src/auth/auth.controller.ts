7f2746acefad94d84806d01bf5aa2c566d1adc2f
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2023-10-29 21:04:04 +0900

    認証機能

--- restapi-todo-nestjs/src/auth/auth.controller.ts
+++ restapi-todo-nestjs/src/auth/auth.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Req,
+  Get,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Csrf, Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signUp(@Body() dto: AuthDto): Promise<Msg> {
+    return this.authService.signUp(dto);
+  }
+
+  @HttpCode(HttpStatus.OK) //デフォルトだとステータスコード201(create)になるため200に変更
+  @Post('login')
+  async login(
+    @Body() dto: AuthDto,
+
+    /**
+     * Express libraryのobject(@Res decoratorでinjection)を使用すると
+     * standard(Nest.jsの機能でリターンのオブジェクトを自動でjson化してくれる機能)が無効化される。
+     * passthrough: trueにするよ両方有効かできる
+     * @Res Response only → Requestは無関係
+     */
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Msg> {
+    const jwt = await this.authService.login(dto);
+
+    res.cookie('access_token', jwt.accessToken, {
+      httpOnly: true,
+      secure: false, //Secure:true → httpsで暗号化された通信のみcookie使用可能
+      sameSite: 'none',
+      path: '/',
+    });
+
+    return {
+      message: 'ok',
+    };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/logout')
+  logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Msg {
+    res.cookie('access_token', '', {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'none',
+      path: '/',
+    });
+
+    return {
+      message: 'ok',
+    };
+  }
+}
